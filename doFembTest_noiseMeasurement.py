02b038adbddf86e666781c8c0cec8a02f76d2647
kirby <kirby@kirby.bnl.gov>
2016-09-29 11:26:04 -0400

    Added test GUI

--- doFembTest_noiseMeasurement.py
+++ doFembTest_noiseMeasurement.py
@@ -7,101 +7,156 @@ import os
 import ntpath
 import glob
 
-#CHECK STATUS AND INITIALIZATION
-print("NOISE MEASUREMENT - CHECKING READOUT STATUS")
-
 #specify location of femb_udp package
-PATH_FEMB_UDP = None 
-if PATH_FEMB_UDP == None:
-    print("Error running doFembTest - PATH_FEMB_UDP not assigned")
-    print(" Please specify the path of the femb_udp package by assigning it to the PATH_FEMB_UDP at the top of this script.")
-    print(" If you have not checked out the femb_udp package, run the following command: git clone https://github.com/kirbybri/femb_udp.git")
-    print(" This script will exit now")
-    sys.exit(0)
-sys.path.append(PATH_FEMB_UDP)
+PATH_FEMB_UDP = None
 
-#import femb_udp modules from femb_udp package
-from femb_rootdata import FEMB_ROOTDATA
-from femb_config import FEMB_CONFIG
-femb_config = FEMB_CONFIG()
-femb_rootdata = FEMB_ROOTDATA()
+class FEMB_TEST:
 
-#check if readout is working
-testData = femb_rootdata.femb.get_data_packets(1)
-if testData == None:
-    print("Error running doFembTest - FEMB is not streaming data.")
-    print(" Turn on and initialize FEMB UDP readout.")
-    print(" This script will exit now")
-    sys.exit(0)
-if len(testData) == 0:
-    print("Error running doFembTest - FEMB is not streaming data.")
-    print(" Turn on and initialize FEMB UDP readout.")
-    print(" This script will exit now")
-    sys.exit(0)
+    def __init__(self):
+        if PATH_FEMB_UDP == None:
+            print("Error running doFembTest - PATH_FEMB_UDP not assigned")
+            print(" Please specify the path of the femb_udp package by assigning it to the PATH_FEMB_UDP at the top of this script.")
+            print(" If you have not checked out the femb_udp package, run the following command: git clone https://github.com/kirbybri/femb_udp.git")
+            print(" This script will exit now")
+            sys.exit(0)
+        sys.path.append(PATH_FEMB_UDP)
 
-#check for analysis executables
-if os.path.isfile('./processNtuple') == False:    
-    print('processNtuple not found, exiting')
-    sys.exit(0)
+        #import femb_udp modules from femb_udp package
+        from femb_rootdata import FEMB_ROOTDATA
+        from femb_config import FEMB_CONFIG
+        self.femb_config = FEMB_CONFIG()
+        self.femb_rootdata = FEMB_ROOTDATA()
 
-if os.path.isfile('./summaryAnalysis_doFembTest_noiseMeasurement') == False:    
-    print('summaryAnalysis_doFembTest_noiseMeasurement not found, exiting')
-    sys.exit(0)
+        #set status variables
+        self.status_check_setup = 0
+        self.status_record_data = 0
+        self.status_do_analysis = 0
+        self.status_archive_results = 0
 
-#MEASUREMENT SECTION
-print("NOISE MEASUREMENT - RECORDING DATA")
+    def check_setup(self):
+        #CHECK STATUS AND INITIALIZATION
+        print("NOISE MEASUREMENT - CHECKING READOUT STATUS")
+        self.status_check_setup = 0
+        #check if readout is working
+        testData = self.femb_rootdata.femb.get_data_packets(1)
+        if testData == None:
+            print("Error running doFembTest - FEMB is not streaming data.")
+            print(" Turn on and initialize FEMB UDP readout.")
+            #print(" This script will exit now")
+            #sys.exit(0)
+            return
+        if len(testData) == 0:
+            print("Error running doFembTest - FEMB is not streaming data.")
+            print(" Turn on and initialize FEMB UDP readout.")
+            #print(" This script will exit now")
+            #sys.exit(0)
+            return
 
-#initialize readout channel range
-femb_rootdata.minchan = 0
-femb_rootdata.maxchan = 127
+        #check for analysis executables
+        if os.path.isfile('./processNtuple') == False:    
+            print('processNtuple not found, run setup.sh')
+            #sys.exit(0)
+            return
+        if os.path.isfile('./summaryAnalysis_doFembTest_noiseMeasurement') == False:    
+            print('summaryAnalysis_doFembTest_noiseMeasurement not found, run setup.sh')
+            #sys.exit(0)
+            return
+        self.status_check_setup = 1
 
-#initialize output filelist
-filelist = open("filelist_doFembTest_noiseMeasurement_" + str(femb_rootdata.date) + ".txt", "w")
-subrun = 0
-for g in range(0,4,1):
-  for s in range(0,4,1):
-    for b in range(0,2,1):
-        #config FE ASICs
-        femb_config.configFeAsic(g,s,b)
-        sleep(0.5)
-        filename = "data/output_femb_rootdata_doFembTest_" + str(femb_rootdata.date) + "_"  + str(subrun) + ".root"
-        print("Recording " + filename)
-        femb_rootdata.filename = filename
-        femb_rootdata.numpacketsrecord = 10
-        femb_rootdata.run = 0
-        femb_rootdata.subrun = subrun
-        femb_rootdata.runtype = 1
-        femb_rootdata.runversion = 0
-        femb_rootdata.par1 = 0
-        femb_rootdata.par2 = 0
-        femb_rootdata.par3 = 0
-        femb_rootdata.gain = g
-        femb_rootdata.shape = s
-        femb_rootdata.base = b
-        femb_rootdata.record_data_run()
-        filelist.write(filename + "\n")
-        subrun = subrun + 1
-filelist.close()
+    def record_data(self):
+        if self.status_check_setup == 0:
+            print("Please run check_setup method before trying to take data")
+            return
+        if self.status_record_data == 1:
+            print("Data already recorded. Reset/restat GUI to begin a new measurement")
+            return
+        #MEASUREMENT SECTION
+        print("NOISE MEASUREMENT - RECORDING DATA")
 
-#ANALYSIS SECTION
-print("NOISE MEASUREMENT - ANALYZING AND SUMMARIZING DATA")
+        #initialize readout channel range
+        self.femb_rootdata.minchan = 0
+        self.femb_rootdata.maxchan = 127
 
-#process data
-newlist = "filelist_processData_doFembTest_noiseMeasurement_" + str(femb_rootdata.date) + ".txt"
-input_file = open(filelist.name, 'r')
-output_file = open( newlist, "w")
-for line in input_file:
-    filename = str(line[:-1])
-    #print filename
-    call(["./processNtuple", str(line[:-1]) ])
-    newname = max(glob.iglob('*.root'), key=os.path.getctime)
-    call(["mv",newname,"data/."])
-    newname = "data/" + newname
-    output_file.write(newname + "\n")
-    print(filename)
-    print(newname)
-input_file.close()
-output_file.close()
+        #initialize output filelist
+        self.filelist = open("filelist_doFembTest_noiseMeasurement_" + str(self.femb_rootdata.date) + ".txt", "w")
+        subrun = 0
+        for g in range(0,1,1):
+          for s in range(0,1,1):
+            for b in range(0,1,1):
+                #config FE ASICs
+                self.femb_config.configFeAsic(g,s,b)
+                sleep(0.5)
+                filename = "data/output_femb_rootdata_doFembTest_" + str(self.femb_rootdata.date) + "_"  + str(subrun) + ".root"
+                print("Recording " + filename)
+                self.femb_rootdata.filename = filename
+                self.femb_rootdata.numpacketsrecord = 10
+                self.femb_rootdata.run = 0
+                self.femb_rootdata.subrun = subrun
+                self.femb_rootdata.runtype = 1
+                self.femb_rootdata.runversion = 0
+                self.femb_rootdata.par1 = 0
+                self.femb_rootdata.par2 = 0
+                self.femb_rootdata.par3 = 0
+                self.femb_rootdata.gain = g
+                self.femb_rootdata.shape = s
+                self.femb_rootdata.base = b
+                self.femb_rootdata.record_data_run()
+                self.filelist.write(filename + "\n")
+                subrun = subrun + 1
+        self.filelist.close()
+        self.status_record_data = 1
 
-#run summary program
-call(["./summaryAnalysis_doFembTest_noiseMeasurement", newlist ])
+    def do_analysis(self):
+        if self.status_record_data == 0:
+            print("Please record data before analysis")
+            return
+        if self.status_do_analysis == 1:
+            print("Analysis already complete")
+            return
+        #ANALYSIS SECTION
+        print("NOISE MEASUREMENT - ANALYZING AND SUMMARIZING DATA")
+
+        #process data
+        self.newlist = "filelist_processData_doFembTest_noiseMeasurement_" + str(self.femb_rootdata.date) + ".txt"
+        input_file = open(self.filelist.name, 'r')
+        output_file = open( self.newlist, "w")
+        for line in input_file:
+            filename = str(line[:-1])
+            #print filename
+            call(["./processNtuple", str(line[:-1]) ])
+            rootfiles = glob.glob('output_processNtuple_output_femb_rootdata_doFembTest_' + str(self.femb_rootdata.date) + '*.root')
+            if len(rootfiles) == 0:
+                print("Processing error detected, needs debugging. Exiting now!")
+                sys.exit(0)
+                #continue
+            newname = max(rootfiles, key=os.path.getctime)
+            call(["mv",newname,"data/."])
+            newname = "data/" + newname
+            output_file.write(newname + "\n")
+            print(filename)
+            print(newname)
+        input_file.close()
+        output_file.close()
+        #run summary program
+        call(["./summaryAnalysis_doFembTest_noiseMeasurement", self.newlist ])
+        self.status_do_analysis = 1
+
+    def archive_results(self):
+        if self.status_do_analysis == 0:
+            print("Please analyze data before archiving results")
+            return
+        if self.status_archive_results == 1:
+            print("Results already archived")
+            return
+        #ANALYSIS SECTION
+        print("NOISE MEASUREMENT - STORE RESULTS IN DATABASE")
+
+def main():
+    femb_test = FEMB_TEST()
+    femb_test.check_setup()
+    femb_test.record_data()
+    femb_test.do_analysis()
+    femb_test.archive_results()
+
+if __name__ == '__main__':
+    main()
---
+++ gui_femb_test.py
@@ -0,0 +1,165 @@
+#!/usr/bin/python3.4
+from gi.repository import Gtk
+from doFembTest_noiseMeasurement import FEMB_TEST
+
+class GUI_WINDOW():
+
+    #GUI window defined entirely in init function
+    def __init__(self):
+
+        #define configuration object
+        self.femb_test = FEMB_TEST()
+
+        #define main GUI window
+        window = Gtk.Window()
+        window.set_title("Test GUI")
+        window.set_default_size(150, 300)
+        window.set_position(Gtk.WindowPosition.CENTER)
+        window.connect('destroy', self.destroy)
+
+        self.main_hbox = Gtk.HBox(True,0)
+        window.add(self.main_hbox)
+
+        #Define general commands column
+        self.define_test_details_column()
+
+        #Define general commands column
+        self.define_general_commands_column()
+
+        #Show GUI
+        window.show_all()
+
+    def define_test_details_column(self):
+        #Define general commands column-----------------------------------
+        frame_cmd = Gtk.Frame()
+        frame_cmd.set_label("Tests Details")
+        self.main_hbox.pack_start(frame_cmd, True, True, 10) 
+
+        vbox_cmd = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=20)
+        frame_cmd.add(vbox_cmd)
+
+        #add operator name label  + associated field to command column
+        test_name_box = Gtk.HBox(True,0)
+        vbox_cmd.pack_start(test_name_box, False, False, 0)
+
+        test_name_label = Gtk.Label("Operator Name")
+        test_name_box.pack_start(test_name_label, True, True, 0)
+
+        self.test_name_entry = Gtk.Entry()
+        self.test_name_entry.set_text("UNKNOWN")
+        test_name_box.pack_start(self.test_name_entry,True, True, 0)
+
+        #add board ID label  + associated field to command column
+        test_boardid_box = Gtk.HBox(True,0)
+        vbox_cmd.pack_start(test_boardid_box, False, False, 0)
+
+        test_boardid_label = Gtk.Label("Board ID")
+        test_boardid_box.pack_start(test_boardid_label, True, True, 0)
+
+        self.test_boardid_entry = Gtk.Entry()
+        self.test_boardid_entry.set_text("UNKNOWN")
+        test_boardid_box.pack_start(self.test_boardid_entry,True, True, 0)
+
+    def define_general_commands_column(self):
+        #Define general commands column-----------------------------------
+        frame_cmd = Gtk.Frame()
+        frame_cmd.set_label("Commands")
+        self.main_hbox.pack_start(frame_cmd, True, True, 10) 
+
+        vbox_cmd = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=20)
+        frame_cmd.add(vbox_cmd)
+
+        #add general command buttons column
+
+        #add check_setup button  + associated field to command column
+        check_setup_box = Gtk.HBox(True,0)
+        vbox_cmd.pack_start(check_setup_box, False, False, 0)
+
+        check_setup_button = Gtk.Button.new_with_label("Check Test Stand")
+        check_setup_button.connect("clicked", self.check_setup)
+        check_setup_box.pack_start(check_setup_button, True, True, 0)
+
+        self.check_setup_entry = Gtk.Entry()
+        self.check_setup_entry.set_text("UNKNOWN")
+        check_setup_box.pack_start(self.check_setup_entry,True, True, 0)
+
+        #add record_data button to command column
+        record_data_box = Gtk.HBox(True,0)
+        vbox_cmd.pack_start(record_data_box, False, False, 0)
+
+        record_data_button = Gtk.Button.new_with_label("Perform Test")
+        record_data_button.connect("clicked", self.record_data)
+        record_data_box.pack_start(record_data_button, True, True, 0)
+
+        self.record_data_entry = Gtk.Entry()
+        self.record_data_entry.set_text("NOT STARTED")
+        record_data_box.pack_start(self.record_data_entry,True, True, 0)
+
+        #add analyze data button to command column
+        do_analysis_box = Gtk.HBox(True,0)
+        vbox_cmd.pack_start(do_analysis_box, False, False, 0)
+
+        do_analysis_button = Gtk.Button.new_with_label("Analyze Data")
+        do_analysis_button.connect("clicked", self.analyze_data)
+        do_analysis_box.pack_start(do_analysis_button, True, True, 0)
+
+        self.do_analysis_entry = Gtk.Entry()
+        self.do_analysis_entry.set_text("NOT STARTED")
+        do_analysis_box.pack_start(self.do_analysis_entry,True, True, 0)
+
+        #add record_data button to command column
+        archive_results_box = Gtk.HBox(True,0)
+        vbox_cmd.pack_start(archive_results_box, False, False, 0)
+
+        archive_results_button = Gtk.Button.new_with_label("Archive Results")
+        archive_results_button.connect("clicked", self.archive_results)
+        archive_results_box.pack_start(archive_results_button, True, True, 0)
+
+        self.archive_results_entry = Gtk.Entry()
+        self.archive_results_entry.set_text("NOT STARTED")
+        archive_results_box.pack_start(self.archive_results_entry,True, True, 0)
+
+        #add quit button to command column
+        quit_button = Gtk.Button.new_with_label("QUIT")
+        quit_button.connect("clicked", self.destroy)
+        vbox_cmd.pack_start(quit_button, False, False, 0)
+
+        #END general commands column-----------------------------------
+
+    def check_setup(self, window):
+        self.femb_test.check_setup()
+        if self.femb_test.status_check_setup == 0:
+            self.check_setup_entry.set_text("FAILED")
+        else:
+            self.check_setup_entry.set_text("COMPLETE")
+
+    def record_data(self, window):
+        self.femb_test.record_data()
+        if self.femb_test.status_record_data == 0:
+            self.record_data_entry.set_text("FAILED")
+        else:
+            self.record_data_entry.set_text("COMPLETE")
+
+    def analyze_data(self, window):
+        self.femb_test.do_analysis()
+        if self.status_do_analysis == 0:
+            self.do_analysis_entry.set_text("FAILED")
+        else:
+            self.do_analysis_entry.set_text("COMPLETE")
+
+    def archive_results(self, window):
+        self.femb_test.archive_results()
+        if status_archive_results == 0:
+            self.archive_results_entry.set_text("FAILED")
+        else:
+            self.archive_results_entry.set_text("COMPLETE")
+
+    def destroy(self, window):
+        Gtk.main_quit()
+
+def main():
+    app = GUI_WINDOW()
+    Gtk.main()
+
+if __name__ == '__main__':
+    main()
